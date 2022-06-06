d2576138e34fa8eb3096842162ce635b1fb3c259
Chibuike Ewenike <sopewenike@gmail.com>
2022-06-06 22:52:42 +0100

    centered the image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -51,7 +51,7 @@ export default function Meme() {
           onChange={handleChange}
         />
         <button className="form--button" onClick={getMemeImage}>
-          Get a new meme image 🖼
+          Pick a new meme image 🖼
         </button>
       </div>
       <div className="meme">
