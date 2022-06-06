496cddadc6cee050037a525a24fc27c3fa481633
Chibuike Ewenike <sopewenike@gmail.com>
2022-06-06 22:20:56 +0100

    corrected version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from "react"
-import Header from "./components/Header"
-import Meme from "./components/Meme"
-/**
- * Challenge: 
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
+import React from "react";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import "./style.css";
 export default function App() {
-    return (
-        <div>
-            <Header />
-            <Meme />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
 }
